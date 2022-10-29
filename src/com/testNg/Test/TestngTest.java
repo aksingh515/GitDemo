cf5cad9cd265e8e290cff9a729321f431f890d0b
ashutosh <akkiashusms@gmail.com>
2022-10-30 01:09:43 +0530

    First Commit

---
+++ src/com/testNg/Test/TestngTest.java
@@ -0,0 +1,18 @@
+package com.testNg.Test;
+
+import org.testng.annotations.Test;
+
+public class TestngTest 
+{
+	@Test
+	public void demo()
+	{
+		System.out.println("Hello TestNg");
+	}
+	
+	@Test
+	public void demo2()
+	{
+		System.out.println("Bye");
+	}
+}
