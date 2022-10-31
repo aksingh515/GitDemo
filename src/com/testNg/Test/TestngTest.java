47b54db240c6c7c047629655ec7797fa7bf137c1
ashutosh <akkiashusms@gmail.com>
2022-11-01 00:55:15 +0530

    This commint done by Assian timezone guy.

--- src/com/testNg/Test/TestngTest.java
+++ src/com/testNg/Test/TestngTest.java
@@ -2,17 +2,18 @@ package com.testNg.Test;
 
 import org.testng.annotations.Test;
 
-public class TestngTest 
-{
+public class TestngTest {
 	@Test
-	public void demo()
-	{
+	public void demo() {
 		System.out.println("Hello TestNg");
+		System.out.println("Hello Java");
+		System.out.println("Hello Selenium");
+		System.out.println("Hello Maven");
+
 	}
-	
+
 	@Test
-	public void demo2()
-	{
+	public void demo2() {
 		System.out.println("Bye");
 	}
 }
