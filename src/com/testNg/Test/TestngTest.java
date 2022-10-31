7aca07eb234e3285b8536e02e48c1b630444ac3d
ashutosh <akkiashusms@gmail.com>
2022-11-01 01:41:43 +0530

    This is US time zone update.

--- src/com/testNg/Test/TestngTest.java
+++ src/com/testNg/Test/TestngTest.java
@@ -8,7 +8,6 @@ public class TestngTest {
 		System.out.println("Hello TestNg");
 		System.out.println("Hello Java");
 		System.out.println("Hello Selenium");
-		System.out.println("Hello Maven");
 
 	}
 
