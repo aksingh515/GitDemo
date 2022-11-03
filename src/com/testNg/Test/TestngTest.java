ad250708c80120d08551d29b9a78d175c990eaf4
ashutosh <akkiashusms@gmail.com>
2022-11-03 19:36:36 +0530

    First commit

---
+++ src/com/testNg/Test/TestngTest.java
@@ -0,0 +1,20 @@
+package com.testNg.Test;
+
+import org.testng.annotations.Test;
+
+public class TestngTest {
+	@Test
+	public void demo() {
+		System.out.println("Hello TestNg");
+		System.out.println("Hello Java");
+		System.out.println("Hello Selenium");
+		System.out.println("Hello SQL");
+		System.out.println("Hello Python");
+
+	}
+
+	@Test
+	public void demo2() {
+		System.out.println("Bye");
+	}
+}
---
+++ src/com/testNg/Test/TestngTest2.java
@@ -0,0 +1,12 @@
+package com.testNg.Test;
+
+import org.testng.annotations.Test;
+
+public class TestngTest2 
+{
+	@Test
+	public void ploan()
+	{
+		System.out.println("Personal Loan");
+	}
+}
